04e31a875a03c64c94a7a4f5f0579f0b78c45b3c
mysoyul <vega2k_@naver.com>
2023-05-31 10:08:34 +0900

    UserController save add

--- src/main/java/com/basic/myspringboot/controller/UserRestController.java
+++ src/main/java/com/basic/myspringboot/controller/UserRestController.java
@@ -1,12 +1,22 @@
 package com.basic.myspringboot.controller;
 
+import com.basic.myspringboot.entity.User;
 import com.basic.myspringboot.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/users")
 public class UserRestController {
     private final UserService userService;
+
+    @PostMapping
+    public Long save(@RequestBody User userEntity) {
+        return userService.insert(userEntity).getId();
+    }
 
 }
