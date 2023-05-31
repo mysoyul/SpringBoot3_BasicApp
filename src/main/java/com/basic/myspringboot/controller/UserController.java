4b16eec46ecb03bcf9627575264b66b29ceadb21
mysoyul <vega2k_@naver.com>
2023-05-31 13:14:29 +0900

    usercontroller update

--- src/main/java/com/basic/myspringboot/controller/UserController.java
+++ src/main/java/com/basic/myspringboot/controller/UserController.java
@@ -1,14 +1,26 @@
 package com.basic.myspringboot.controller;
 
+import com.basic.myspringboot.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class UserController {
+    private final UserService userService;
+
+    @GetMapping("/index")
+    public String index(Model model) {
+        model.addAttribute("users", userService.selectAll());
+        return "index";
+    }
+
+
     @GetMapping("/leaf")
     public String leaf(Model model) {
-        model.addAttribute("name","basic");
+        model.addAttribute("name","타임리프");
         return "leaf";
     }
 
